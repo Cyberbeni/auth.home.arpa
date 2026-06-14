83795f09fe38badf11671dd3e456feb22c6aad65
Benedek Kozma <cyberbeni@gmail.com>
2026-06-14 19:33:47 +0200

    Fix Proton Pass not autofilling, finetune mobile layout

--- Sources/auth_home_arpa/HTML/Pages/LoginPage.swift
+++ Sources/auth_home_arpa/HTML/Pages/LoginPage.swift
@@ -14,30 +14,32 @@ struct LoginPage: HTMLDocument {
 	}
 
 	var body: some HTML {
-		form(.class("login-form grid"), .hx.post("/api/login")) {
-			// TODO: doesn't work with Proton Pass
-			label {
-				"User:"
+		form(.hx.post("/api/login")) {
+			// Proton Pass doesn't autofill if the labels and inputs are at the root of the form
+			div(.class("login-form grid")) {
+				label {
+					"User:"
+				}
+				input(
+					.type(.text),
+					.autocomplete("username"),
+					.id("user"),
+					.name("user"),
+					.init(name: "autocapitalize", value: "off"),
+					.required,
+				)
+				label {
+					"Password:"
+				}
+				input(
+					.type(.password),
+					.autocomplete("current-password"),
+					.id("password"),
+					.name("password"),
+					.required,
+				)
+				input(.type(.submit), .value("Login"), .id("login-button"))
 			}
-			input(
-				.type(.text),
-				.autocomplete("username"),
-				.id("user"),
-				.name("user"),
-				.init(name: "autocapitalize", value: "off"),
-				.required,
-			)
-			label {
-				"Password:"
-			}
-			input(
-				.type(.password),
-				.autocomplete("current-password"),
-				.id("password"),
-				.name("password"),
-				.required,
-			)
-			input(.type(.submit), .value("Login"), .id("login-button"))
 		}
 	}
 }
